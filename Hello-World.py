a6ce4c334351ac310dff7c6027dd7e0ec70237f4
anupkumarn <anup_kumar_n@outlook.com>
2018-05-28 21:26:28 -0700

    aaa

--- Hello-World.py
+++ Hello-World.py
@@ -1,12 +1,5 @@
 print ("hello world 2")
 print ("hello world")
-<<<<<<< HEAD
 print ("all set?")
-=======
 print ("i am ready")
-<<<<<<< HEAD
 print ("final check")
->>>>>>> fixbr
-=======
-print ("final check")
->>>>>>> fixbr
