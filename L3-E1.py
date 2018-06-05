5c41a2fec116414aad82459b361d70075b299506
anupkumarn <anup_kumar_n@outlook.com>
2018-06-04 21:25:07 -0700

    L3

--- L3-E1.py
+++ L3-E1.py
@@ -1,5 +1,6 @@
 #yield allows you to return a variable without losing the local variable values. In the example below yield is called within a function 
 #where it returns the value to the calling function without losing track of the function variables.
+#yield is a generator which preserves function local values.
 
 def new_range(start,end,step):
     while start <= end:
